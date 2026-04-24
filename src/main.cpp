fe0eab5e185a03384e7d992dbb3ddd8eefc76580
Eivan <eivan.ecedillo@gmail.com>
2026-04-23 18:15:51 -0600

    Modulo neopixel y fiesta planta baja implementado

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,37 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <ESP32Servo.h>
+#include <Adafruit_NeoPixel.h>
 
-// --- PINES DEL ESCLAVO ---
-#define LED1_PIN 25 // Pasillo
-#define LED2_PIN 26 // Pasillo
-#define LED3_PIN 27 // Pasillo
+#define PIN_SERVO_DER 14
+#define PIN_SERVO_IZQ 13
+#define LED1_PIN 25
+#define LED2_PIN 26
+#define LED3_PIN 27
+#define LED_ENTRADA 33
+#define PIN_NEOPIXEL 18
+#define PIN_SERVO_DISCO 19
 
-#define PIN_SERVO_IZQ 13 // Puerta Izquierda
-#define PIN_SERVO_DER 14 // Puerta Derecha
-#define LED_ENTRADA 33   // Foco de la entrada
+// --- AJUSTES SERVOS ENTRADA ---
+int cerradoIzq = 0, abiertoIzq = 80;
+int cerradoDer = 180, abiertoDer = 100;
+bool estadoPuertasActual = false;
+Servo servoIzq, servoDer;
 
-// --- AJUSTES EDITABLES (SERVOS) ---
-int cerradoIzq = 0;
-int abiertoIzq = 80;
+// --- AJUSTES MODO FIESTA ---
+#define NUM_LEDS 16
+Adafruit_NeoPixel tira = Adafruit_NeoPixel(NUM_LEDS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+Servo servoDisco;
+int minAnguloDisco = 20, maxAnguloDisco = 55, anguloDisco = 37;
+bool subiendoDisco = true;
+unsigned long tiempoUltimoColor = 0;
+bool oficinaPintada = false;
 
-int cerradoDer = 180;
-int abiertoDer = 100;
-
-Servo servoIzq;
-Servo servoDer;
-
-// ¡LA CAJA DEBE SER IDÉNTICA AL MAESTRO!
 typedef struct struct_message
 {
     bool presenciaPasillo;
     bool presenciaEntrada;
+    bool fiestaActiva;
 } struct_message;
 
 struct_message datosRecibidos;
@@ -34,72 +40,95 @@ struct_message datosRecibidos;
 void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
     memcpy(&datosRecibidos, incomingData, sizeof(datosRecibidos));
-
-    // 1. REACCIONAR AL PASILLO
-    if (datosRecibidos.presenciaPasillo)
-    {
-        digitalWrite(LED1_PIN, HIGH);
-        digitalWrite(LED2_PIN, HIGH);
-        digitalWrite(LED3_PIN, HIGH);
-    }
-    else
-    {
-        digitalWrite(LED1_PIN, LOW);
-        digitalWrite(LED2_PIN, LOW);
-        digitalWrite(LED3_PIN, LOW);
-    }
-
-    // 2. REACCIONAR A LA ENTRADA
-    if (datosRecibidos.presenciaEntrada)
-    {
-        // Alguien llegó: Abrir puertas y prender luz
-        servoIzq.write(abiertoIzq);
-        servoDer.write(abiertoDer);
-        digitalWrite(LED_ENTRADA, HIGH);
-    }
-    else
-    {
-        // No hay nadie: Cerrar puertas y apagar luz
-        servoIzq.write(cerradoIzq);
-        servoDer.write(cerradoDer);
-        digitalWrite(LED_ENTRADA, LOW);
-    }
 }
 
 void setup()
 {
     Serial.begin(115200);
+    tira.begin();
+    tira.show();
 
-    // Configurar LEDs
     pinMode(LED1_PIN, OUTPUT);
     pinMode(LED2_PIN, OUTPUT);
     pinMode(LED3_PIN, OUTPUT);
     pinMode(LED_ENTRADA, OUTPUT);
 
-    // Configurar Servos
     ESP32PWM::allocateTimer(0);
     ESP32PWM::allocateTimer(1);
-    servoIzq.setPeriodHertz(50);
-    servoDer.setPeriodHertz(50);
+    ESP32PWM::allocateTimer(2);
+
     servoIzq.attach(PIN_SERVO_IZQ, 500, 2400);
     servoDer.attach(PIN_SERVO_DER, 500, 2400);
+    servoDisco.attach(PIN_SERVO_DISCO, 500, 2400);
 
-    // Estado inicial: Todo apagado y puertas cerradas
-    digitalWrite(LED1_PIN, LOW);
-    digitalWrite(LED2_PIN, LOW);
-    digitalWrite(LED3_PIN, LOW);
-    digitalWrite(LED_ENTRADA, LOW);
     servoIzq.write(cerradoIzq);
     servoDer.write(cerradoDer);
 
-    // Iniciar ESP-NOW
     WiFi.mode(WIFI_STA);
-    if (esp_now_init() != ESP_OK)
-        return;
+    esp_now_init();
     esp_now_register_recv_cb(esp_now_recv_cb_t(onDataRecv));
 }
 
 void loop()
 {
-    // El Esclavo sigue limpio, todo se controla en onDataRecv
+    unsigned long tiempoActual = millis();
+
+    // 1. PASILLO
+    digitalWrite(LED1_PIN, datosRecibidos.presenciaPasillo);
+    digitalWrite(LED2_PIN, datosRecibidos.presenciaPasillo);
+    digitalWrite(LED3_PIN, datosRecibidos.presenciaPasillo);
+
+    // 2. ENTRADA
+    if (datosRecibidos.presenciaEntrada != estadoPuertasActual)
+    {
+        estadoPuertasActual = datosRecibidos.presenciaEntrada;
+        if (estadoPuertasActual)
+        {
+            servoIzq.write(abiertoIzq);
+            servoDer.write(abiertoDer);
+            digitalWrite(LED_ENTRADA, HIGH);
+        }
+        else
+        {
+            servoIzq.write(cerradoIzq);
+            servoDer.write(cerradoDer);
+            digitalWrite(LED_ENTRADA, LOW);
+        }
+    }
+
+    // 3. FIESTA (Sin Buzzer para evitar conflictos)
+    if (datosRecibidos.fiestaActiva)
+    {
+        oficinaPintada = false;
+        if (tiempoActual - tiempoUltimoColor >= 1000)
+        {
+            for (int i = 0; i < NUM_LEDS; i++)
+            {
+                tira.setPixelColor(i, tira.Color(random(255), random(255), random(255)));
+            }
+            tira.show();
+            tiempoUltimoColor = tiempoActual;
+        }
+        if (subiendoDisco)
+            anguloDisco += 2;
+        else
+            anguloDisco -= 2;
+        if (anguloDisco >= maxAnguloDisco)
+            subiendoDisco = false;
+        if (anguloDisco <= minAnguloDisco)
+            subiendoDisco = true;
+        servoDisco.write(anguloDisco);
+    }
+    else
+    {
+        if (!oficinaPintada)
+        {
+            for (int i = 0; i < NUM_LEDS; i++)
+                tira.setPixelColor(i, tira.Color(5, 5, 5));
+            tira.show();
+            oficinaPintada = true;
+        }
+        servoDisco.write(37);
+    }
+    delay(20);
 }
